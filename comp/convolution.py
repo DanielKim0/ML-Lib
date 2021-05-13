da132e30030dcdce562fac9b49fce0a92c960b38
Daniel Kim <danielynskim@gmail.com>
2021-05-13 13:16:35 -0400

    Add padding/stride for pooling and fix stride for conv

--- comp/convolution.py
+++ comp/convolution.py
@@ -4,13 +4,13 @@ def conv2d(X, K, padding, stride):
     h, w = K.shape
     if padding == "same":
         X = tf.pad(X, [[h-1, h-1], [w-1, w-1]])
-    return conv2d_calc(X, k, padding)
+    return conv2d_calc(X, k, stride)
 
-def conv2d_calc(X, K, padding):
+def conv2d_calc(X, K, stride):
     h, w = K.shape
     Y = tf.Variable(tf.zeros((X.shape[0]-h+1, X.shape[1]-w+1)))
-    for i in range(0, Y.shape[0], padding[0]):
-        for j in range(0, Y.shape[1], padding[1]):
+    for i in range(0, Y.shape[0], stride[0]):
+        for j in range(0, Y.shape[1], stride[1]):
             Y[i,j].assign(tf.reduce_sum(X[i:i+h,j:j+w] * K))
     return Y
 
--- comp/pooling.py
+++ comp/pooling.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 
-def pooling2d(X, size, mode="max"):
+def pooling2d(X, size, padding, stride, mode="max"):
     h, w = size
+    if padding == "same":
+        X = tf.pad(X, [[h-1, h-1], [w-1, w-1]])
+
     Y = tf.Variable(tf.zeros((X.shape[0]-h+1, X.shape[1]-w+1)))
-    for i in range(0, Y.shape[0]):
-        for j in range(0, Y.shape[1]):
+    for i in range(0, Y.shape[0], stride[0]):
+        for j in range(0, Y.shape[1], stride[1]):
             if mode == "max":
                 tf.reduce_max(X[i:i+h, j:j+w])
             elif mode == "avg":
