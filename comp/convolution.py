e03d9af9a090a4e8f91ef082cea68ed4eb167def
Daniel Kim <danielynskim@gmail.com>
2021-05-12 22:51:58 -0400

    Add multichannel 2d convolution operators

--- comp/convolution.py
+++ comp/convolution.py
@@ -13,3 +13,17 @@ def conv2d_calc(X, K, padding):
         for j in range(0, Y.shape[1], padding[1]):
             Y[i,j].assign(tf.reduce_sum(X[i:i+h,j:j+w] * K))
     return Y
+
+def conv2d_multi(X, kernel, padding, stride):
+    channels = []
+    for K in kernel:
+        channels.append(tf.reduce_sum([conv2d(x, k, padding, stride) for x, k in zip(X, K)], axis=0))
+    return tf.stack(channels, 0)
+
+def conv2d_multi_1x1(X, K):
+    conv_in, h, w = X.shape
+    conv_out = K.shape[0]
+    X = tf.reshape(X, (conv_in, h * w))
+    K = tf.reshape(K, (conv_out, conv_in))
+    Y = tf.matmul(K, X)
+    return tf.reshape(Y, (conv_out, h, w))
